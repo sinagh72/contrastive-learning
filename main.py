2c7ab130ebcef4773fab62bc2abb2b91367816cf
sinagh72 <sina.gholami72@gmail.com>
2022-11-15 12:49:32 +0100

    ready to train contrastive learning

---
+++ main.py
@@ -0,0 +1,65 @@
+import os
+import matplotlib.pyplot as plt
+import torchvision
+
+from custom_dataset import CustomDataset, ContrastiveTransformations, train_aug
+from train import train_simclr
+
+plt.set_cmap('cividis')
+import matplotlib
+
+matplotlib.rcParams['lines.linewidth'] = 2.0
+import seaborn as sns
+
+sns.set()
+## PyTorch
+import torch
+import torch.utils.data as data
+
+
+def show_img(num_imgs=6, n_views=2):
+    imgs = torch.stack([img for idx in range(num_imgs) for img in train[idx]["img"]], dim=0)
+    img_grid = torchvision.utils.make_grid(imgs, nrow=n_views, normalize=True, pad_value=0.9)
+    img_grid = img_grid.permute(1, 2, 0)
+
+    plt.figure(figsize=(10, 5))
+    plt.title('Augmented image examples of the 2014 BOE Srinivasan2 dataset')
+    plt.imshow(img_grid)
+    plt.axis('off')
+    plt.show()
+    plt.close()
+
+
+if __name__ == "__main__":
+    # Path to the folder where the datasets are/should be downloaded (e.g. CIFAR10)
+    DATASET_PATH = "./2014_BOE_Srinivasan_2/Publication_Dataset"
+    # Path to the folder where the pretrained models are saved
+    CHECKPOINT_PATH = "./saved_models"
+    # In this notebook, we use data loaders with heavier computational processing. It is recommended to use as many
+    # workers as possible in a data loader, which corresponds to the number of CPU cores
+    NUM_WORKERS = os.cpu_count()
+
+    # Ensure that all operations are deterministic on GPU (if used) for reproducibility
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    print("Device:", device)
+    print("Number of workers:", NUM_WORKERS)
+
+    n_views = 2
+
+    train_dataset = CustomDataset(data_root=DATASET_PATH, mode="train", img_suffix='.tif',
+                                  transform=ContrastiveTransformations(train_aug, n_views=n_views))
+
+    train, valid = torch.utils.data.random_split(train_dataset, [0.7, 0.3], generator=torch.Generator().manual_seed(42))
+
+    simclr_model = train_simclr(batch_size=2,
+                                max_epochs=500,
+                                train_data=train,
+                                val_data=valid,
+                                checkpoint_path=CHECKPOINT_PATH,
+                                hidden_dim=128,
+                                lr=5e-4,
+                                temperature=0.07,
+                                weight_decay=1e-4)
---
+++ train.py
@@ -0,0 +1,46 @@
+import os
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+import torch
+from torch.utils.data import DataLoader
+
+from simclr import SimCLR
+
+NUM_WORKERS = 4
+
+
+def train_simclr(batch_size, max_epochs=500, train_data=None, val_data=None, checkpoint_path=None, **kwargs):
+    # Setting the seed
+    pl.seed_everything(42)
+
+    # Ensure that all operations are deterministic on GPU (if used) for reproducibility
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    trainer = pl.Trainer(default_root_dir=os.path.join(checkpoint_path, 'SimCLR'),
+                         accelerator="gpu" if str(device).startswith("cuda") else "cpu",
+                         devices=1,
+                         max_epochs=max_epochs,
+                         callbacks=[ModelCheckpoint(save_weights_only=True, mode='max', monitor='val_acc_top5'),
+                                    LearningRateMonitor('epoch')])
+    trainer.logger._default_hp_metric = None  # Optional logging argument that we don't need
+
+    # Check whether pretrained model exists. If yes, load it and skip training
+    pretrained_filename = os.path.join(checkpoint_path, 'SimCLR.ckpt')
+    if os.path.isfile(pretrained_filename):
+        print(f'Found pretrained model at {pretrained_filename}, loading...')
+        model = SimCLR.load_from_checkpoint(
+            pretrained_filename)  # Automatically loads the model with the saved hyperparameters
+    else:
+        train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True,
+                                  drop_last=True, pin_memory=True, num_workers=NUM_WORKERS)
+        val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False,
+                                drop_last=False, pin_memory=True, num_workers=NUM_WORKERS)
+
+        model = SimCLR(max_epochs=max_epochs, **kwargs)
+        trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+        model = SimCLR.load_from_checkpoint(
+            trainer.checkpoint_callback.best_model_path)  # Load best checkpoint after training
+
+    return model
