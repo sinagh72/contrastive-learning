70477a8e2c28a76989b5f0ca5ed49024b6c6d128
sgholami <sgholami@uncc.edu>
2023-02-13 17:18:42 +0000

    update

--- kaggle_main.py
+++ kaggle_main.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     # Path to the folder where the datasets are
     DATASET_PATH = os.getenv('KAGGLE_MAIN_DATASET_PATH')
     # Path to the folder where the pretrained models are saved
-    CHECKPOINT_PATH = "./kaggle_saved_models_full_2cores/SimCLR/"
+    CHECKPOINT_PATH = "./kaggle_saved_models_full_2cores_acc/SimCLR/"
     # Path to style transferred images
     # NST_PATH = "data/nst_full.hdf5"
     # In this notebook, we use data loaders with heavier computational processing. It is recommended to use as many
@@ -91,6 +91,6 @@ if __name__ == "__main__":
                                     weight_decay=1e-4,
                                     n_views=N_VIEWS,
                                     save_model_name="SimCLR_" + str(i),
-                                    monitor="val_loss_epoch",
-                                    mode="min"
+                                    monitor="val_acc_top5_epoch",
+                                    mode="max"
                                     )
