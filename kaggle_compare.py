a93de4b63c1645886e96dcfbe33e9f7a23ecf7f7
sinagh72 <sina.gholami72@gmail.com>
2023-02-13 01:19:58 +0000

    compare is done core 8

--- kaggle_compare.py
+++ kaggle_compare.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     # Path to the folder where the datasets are
     DATASET_PATH = os.getenv('KAGGLE_COMPARE_DATASET_PATH')
     # Path to load simclr and to save resnet and linear models
-    CHECKPOINT_PATH = "./kaggle_saved_models_full_2cores/"
+    CHECKPOINT_PATH = "./kaggle_saved_models_full_8cores/"
     # Path to style transferred image
     # NST_PATH = "data/nst_full.hdf5"
 
@@ -47,7 +47,7 @@ if __name__ == "__main__":
                ("DME", 2)]
 
     metric = "accuracy"
-    log_name_suffix = "kaggle_full_2cores"
+    log_name_suffix = "kaggle_full_8cores"
     batch_size = 128
 
     for i in range(CV):
