c474d9987aae4fa205a06c51821a01efd77d28b2
sinagh72 <sina.gholami72@gmail.com>
2023-02-15 15:17:15 +0000

    remove

--- kaggle_main.py
+++ kaggle_main.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     # Path to the folder where the datasets are
     DATASET_PATH = os.getenv('KAGGLE_BALANCED_DATASET_PATH')
     # Path to the folder where the pretrained models are saved
-    CHECKPOINT_PATH = "./kaggle_saved_models_balanced_8cores_acc/SimCLR/"
+    CHECKPOINT_PATH = "./kaggle_saved_models_balanced_8cores/SimCLR/"
     # Path to style transferred images
     # NST_PATH = "data/nst_full.hdf5"
     # In this notebook, we use data loaders with heavier computational processing. It is recommended to use as many
@@ -91,6 +91,6 @@ if __name__ == "__main__":
                                     weight_decay=1e-4,
                                     n_views=N_VIEWS,
                                     save_model_name="SimCLR_" + str(i),
-                                    monitor="val_acc_top5_epoch",
-                                    mode="max"
+                                    monitor="val_loss_epoch",
+                                    mode="min"
                                     )
